c27448c8cf89a6b24e313166a24d7485749869ed
Sofeya2610 <sofiia.ostrovska.oi.2024@lpnu.ua>
2026-04-21 16:52:35 +0300

    fixed routing and homepage for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 
